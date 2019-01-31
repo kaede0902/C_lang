96f47507fb09eb3768ce0cb2043fb99ee6aa89c3
kaede0902 <kaede0902js@gmail.com>
2019-01-31 19:10:44 +0900

    update mistake

--- C_pictureBook/U05/P98.c
+++ C_pictureBook/U05/P98.c
@@ -9,25 +9,26 @@ int main(int argc, char const *argv[])
     char newFileName[256] = "";
 
     if (argc <= 1) {
-        printf(usage);
+        printf("%s",usage);
         return 1;
     }
-    else if (strcmp(argv[1], "-d")== 0) {
+    else if (strcmp(argv[2], "-d")== 0) {
         if (argc <= 2) {
-            printf("usage");
+            printf("%s",usage);
             return 2;
         }
-        rtn = remove(argv[2]);
+        rtn = remove(argv[3]);
         if (rtn == 0)
             printf("File Deleted. \n");
         else
             printf("File Cannot Deleted. \n");
     }
     else {
-        sprintf(newFileName, "%s.bak", argv[1]);
-        rtn = rename(argv[1],newFileName);
+        sprintf(newFileName, "%s.bak", argv[2]);
+        rtn = rename(argv[2],newFileName);
         if (rtn == 0)
-            printf("Add .bak ");
+            printf("Add .bak \n");
+        else 
+            printf("Cannot change flie name\n");
     }
-    
 }
